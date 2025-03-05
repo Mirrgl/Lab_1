7056d4d6f1ea94db175f671fd512975fcb282329
Mirrgl <dima.obmetko.06@gmail.com>
2025-03-05 09:06:46 +0700

    change circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -7,10 +7,18 @@ int main() {
     float radius = 0;
     cout << "Введите радиус окружности: ";
     cin >> radius;
+    if (radius <= 0) {
+        cout << "Ошибка. Введено неположительное число" << endl;
+        return 0;
+    }
 
     float angle = 0;
     cout << "Введите угол кругового сектора окружности (в градусах): ";
     cin >> angle;
+    if (angle <= 0) {
+        cout << "Ошибка. Введено неположительное число" << endl;
+        return 0;
+    }
 
     float length = 2 * M_PI * radius;
     cout << "Длина окружности: " << length << endl;
